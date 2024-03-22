c69ff0c12673bc3b4047bff22b310a93569561c2
evgeniymoiseev <evgeniy.moiseev.1985@gmail.com>
2024-03-22 14:18:52 +0200

    1616

--- src/main/kotlin/com/rainbowgames/Application.kt
+++ src/main/kotlin/com/rainbowgames/Application.kt
@@ -5,10 +5,10 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-const val GADID_KEY = "mu9wu3d9le"
-const val DEEPLINK_KEY = "24e3i0p9lt"
-const val HOST = "starsshinyday.quest"
-const val SEGMENT = "wod31u.php"
+const val GADID_KEY = "fyvee7fg34"
+const val DEEPLINK_KEY = "j3szvclsn7"
+const val HOST = "classicstoryfromegypt.online"
+const val SEGMENT = "ss2bumyv8.php"
 
 fun main() {
     embeddedServer(Netty, port = System.getenv("PORT")?.toInt() ?: 8080, module = Application::module)
